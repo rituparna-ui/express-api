d1476ba8d2af33e2766ac665fbab3678d5b7d56e
Ritu <rwarwatkar@gmail.com>
2020-09-03 19:03:51 +0530

    complete with crud

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,7 +1,45 @@
- 
+let users = [];
+const { v4: uuidv4 } = require('uuid');
 const getAll = (req,res)=>{
         res.json(users);
         res.end();
 }
 
+const createUser = (req,res)=>{
+        const uId = uuidv4();
+        const user = req.body;
+        const newUserWithId ={...user,id:uId}
+        users.push(newUserWithId);
+        res.end();
+}
+
+const getById = (req,res)=>{
+        const { id } = req.params;
+        const foundUser = users.find((user)=> user.id === id);
+        res.json(foundUser);
+        res.end();
+}
+
+const deleteUser = (req,res)=>{
+        const { id } = req.params;
+        users = users.filter((user)=> user.id !== id);
+        res.json(users)
+}
+
+const patchUser = (req,res)=>{
+        const { id } = req.params;
+        const userUpdate = users.find((user)=> user.id === id);
+        const { first , last , age } = req.body;
+        if(first)userUpdate.first = first;
+        if(last)userUpdate.last = last;
+        if(age)userUpdate.age = age;
+        res.end();
+
+}
+
+
+exports.patchUser = patchUser;
+exports.deleteUser = deleteUser;
+exports.getById = getById;
 exports.getAll = getAll;
+exports.createUser = createUser;
--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
-const { getAll } = require("./../controllers/userControl");
+const { getAll, createUser, getById, deleteUser, patchUser } = require("./../controllers/userControl");
 
 let users = [];
 
 router.get("/",getAll);
 
-router.post("/",(req,res)=>{
-	const uId = uuidv4();
-	const user = req.body;
-	const newUserWithId ={...user,id:uId}
-	users.push(newUserWithId);
-	res.end();
-});
+router.post("/",createUser);
 
-router.get("/:id",(req,res)=>{
-	const { id } = req.params;
-	const foundUser = users.find((user)=> user.id === id);
-	res.json(foundUser);
-	res.end();
-});
+router.get("/:id",getById);
 
-router.delete("/:id",(req,res)=>{
-	const { id } = req.params;
-	users = users.filter((user)=> user.id !== id);
-	res.json(users)
-});
+router.delete("/:id",deleteUser);
 
-router.patch("/:id",(req,res)=>{
-	const { id } = req.params;
-
-	const userUpdate = users.find((user)=> user.id === id);
-	const { first , last , age } = req.body;
-
-	if(first)userUpdate.first = first;
-	if(last)userUpdate.last = last;
-	if(age)userUpdate.age = age;
-	res.end();
-
-});
+router.patch("/:id",patchUser);
 
 module.exports = router;
